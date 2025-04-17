37a6576eb30ef6b3b8c4d74f18ceece9f214cf99
Roman Novikov <rno@tassta.com>
2025-04-17 14:17:06 +0400

    Up

--- src/components/BottomMenu.tsx
+++ src/components/BottomMenu.tsx
@@ -40,6 +40,7 @@ const BottomMenu = () => {
         alignItems: "center",
         padding: "0 16px",
         borderTop: "1px solid #e0e0e0",
+        paddingBottom: "10px"
       }}
     >
       <Button
